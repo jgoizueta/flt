f542683051658d48d5a12255fee7a5c784b15a7c
Javier Goizueta <jgoizueta@gmail.com>
2009-06-29 19:42:33 +0200

    New tests for BinFloat arithmetic.

---
+++ test/test_bin_arithmetic.rb
@@ -0,0 +1,72 @@
+require File.dirname(__FILE__) + '/helper.rb'
+
+# These tests assume that Float arithmetic is correctly rounded
+# Random tests using Float as a reference
+class TestBinArithmetic < Test::Unit::TestCase
+
+  def setup
+    initialize_context
+  end
+
+  def test_addition
+    float_emulation_context
+    srand 93831
+    1000.times do
+      x = random_float
+      y = random_float
+      z = x + y
+      assert_equal z, (BinFloat(x)+BinFloat(y)).to_f
+    end
+  end
+
+  def test_subtraction
+    float_emulation_context
+    srand 93831
+    1000.times do
+      x = random_float
+      y = random_float
+      z = x - y
+      assert_equal z, (BinFloat(x)-BinFloat(y)).to_f
+    end
+  end
+
+  def test_multiplication
+    float_emulation_context
+    srand 93831
+    1000.times do
+      x = random_float
+      y = random_float
+      z = x * y
+      assert_equal z, (BinFloat(x)*BinFloat(y)).to_f
+    end
+  end
+
+  def test_division
+    float_emulation_context
+    srand 93831
+    1000.times do
+      x = random_float
+      y = random_float
+      # next if y.abs < Float::EPSILON*x.abs
+      z = x / y
+      if z != (BinFloat(x)/BinFloat(y)).to_f
+        puts "x=#{float_split(x).inspect}"
+        puts "y=#{float_split(y).inspect}"
+        puts "z=#{float_split(z).inspect}"
+        puts "->#{(BinFloat(x)/BinFloat(y)).split.inspect}"
+      end
+      assert_equal z, (BinFloat(x)/BinFloat(y)).to_f
+    end
+  end
+
+  def test_sqrt
+    float_emulation_context
+    srand 93831
+    1000.times do
+      x = random_float.abs
+      z = Math.sqrt(x)
+      assert_equal z, BinFloat(x).sqrt.to_f
+    end
+  end
+
+end
--- test/test_binfloat_conversion.rb
+++ test/test_binfloat_conversion.rb
@@ -6,24 +6,13 @@ class TestBinfloatConversion < Test::Unit::TestCase
     initialize_context
   end
 
-  def rand_in(min, max)
-    n = max - min + 1
-    rand(n) + min
-  end
-
   def test_conversions
-    BinFloat.context.precision = Float::MANT_DIG
-    BinFloat.context.emin = Float::MIN_EXP-1
-    BinFloat.context.emax = Float::MAX_EXP-1
+    float_emulation_context
     srand 12322
     [:half_even, :half_up, :half_down, :down, :up, :floor, :ceiling].each do |rounding|
       BinFloat.context.rounding = rounding
       1000.times do
-        f = rand(2**Float::MANT_DIG)
-        f = -f if rand(1)==0
-        e = rand_in(Float::MIN_EXP-Float::MANT_DIG, Float::MAX_EXP-Float::MANT_DIG)
-        x = Math.ldexp(f, e)
-
+        x = random_float
         txt = BinFloat(x).to_s
         y = BinFloat(txt).to_f
         assert_equal x, y
