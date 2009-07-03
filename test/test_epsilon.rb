fae965273ad0ba71d9d38f19c65f8d1748906283
Javier Goizueta <jgoizueta@gmail.com>
2009-07-03 20:16:21 +0200

    Extend epsilon tests to BinFloat.

--- test/test_epsilon.rb
+++ test/test_epsilon.rb
@@ -13,21 +13,42 @@ class TestEpsilon < Test::Unit::TestCase
     assert_equal Decimal(1,1,1-Decimal.context.precision), eps
   end
 
-  def test_strict_epsilon
+  def test_epsilon_bin
+    eps = BinFloat.context.epsilon
+    assert_equal((BinFloat(1).next_plus - BinFloat(1)), eps)
+    assert_equal BinFloat(1,1,1-BinFloat.context.precision), eps
+  end
 
+  def test_strict_epsilon
     [:up, :ceiling, :down, :floor, :half_up, :half_down, :half_even, :up05].each do |rounding|
       Decimal.context.rounding = rounding
       eps = Decimal.context.strict_epsilon
       eps_1 = Decimal(1)+eps
       r = eps.next_minus
       r_1 = Decimal(1)+r
-      assert((eps_1 > Decimal(1)) && (r_1 == Decimal(1)), "Test stritct epsilon for rounding #{rounding}")
+      assert((eps_1 > Decimal(1)) && (r_1 == Decimal(1)), "Test strict epsilon for rounding #{rounding}")
       assert_equal(((Decimal(1)+eps)-Decimal(1)), Decimal.context.epsilon)
+    end
+  end
+
+  def test_strict_epsilon_bin
+    [:up, :ceiling, :down, :floor, :half_up, :half_down, :half_even].each do |rounding|
+      BinFloat.context.rounding = rounding
+      eps = BinFloat.context.strict_epsilon
+      eps_1 = BinFloat(1)+eps
+      r = eps.next_minus
+      r_1 = BinFloat(1)+r
+      assert((eps_1 > BinFloat(1)) && (r_1 == BinFloat(1)), "Test strict binary epsilon for rounding #{rounding}")
+      assert_equal(((BinFloat(1)+eps)-BinFloat(1)), BinFloat.context.epsilon)
     end
   end
 
   def test_half_epsilon
     assert_equal Decimal.context.epsilon/2, Decimal.context.half_epsilon
+  end
+
+  def test_half_epsilon_bin
+    assert_equal BinFloat.context.epsilon/2, BinFloat.context.half_epsilon
   end
 
 
