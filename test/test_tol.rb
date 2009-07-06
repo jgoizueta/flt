3983b250abfd91938e61c167ccc3c52eccf27c09
Javier Goizueta <jgoizueta@gmail.com>
2009-07-06 18:28:21 +0200

    Add some Tolerance tests.

--- test/test_tol.rb
+++ test/test_tol.rb
@@ -1,11 +1,12 @@
 require File.dirname(__FILE__) + '/helper.rb'
-require File.dirname(__FILE__) + '/../../lib/bigfloat/tolerance'
+require File.dirname(__FILE__) + '/../lib/bigfloat/tolerance'
 
 class TestTolerance < Test::Unit::TestCase
 
 
   def setup
     initialize_context
+    float_emulation_context
 
     Decimal.context.define_conversion_from(Float) do |x, dec_context|
       BinFloat.context(:rounding=>dec_context.rounding) do |bin_context|
@@ -25,19 +26,74 @@ class TestTolerance < Test::Unit::TestCase
 
   end
 
+  def test_absolute
+
+    tol = Tolerance(100, :absolute)
+    assert_equal 100, tol.value(1.0)
+    assert_equal 100, tol.value(1.5)
+    assert_equal 100, tol.value(1.0E10)
+    assert_equal 100, tol.value(-1.0E10)
+    assert_equal 100, tol.value(Decimal('1.0'))
+    assert_equal 100, tol.value(Decimal('1.5'))
+    assert_equal 100, tol.value(Decimal('1.0E10'))
+    assert_equal 100, tol.value(Decimal('-1.0E10'))
+    assert tol.eq?(11234.0, 11280.0)
+    assert tol.eq?(11234.0, 11135.0)
+    assert tol.eq?(-11234.0, -11280.0)
+    assert tol.eq?(Decimal('11234.0'), Decimal('11280.0'))
+    assert tol.eq?(Decimal('11234.0'), Decimal('11135.0'))
+    assert tol.eq?(Decimal('-11234.0'), Decimal('-11280.0'))
+    assert tol.eq?(Decimal('-11234.0'), Decimal('-11135.0'))
+    assert !tol.eq?(11234.0, -11280.0)
+    assert !tol.eq?(-11234.0, 11280.0)
+    assert !tol.eq?(11234.0, 11335.0)
+    assert !tol.eq?(11234.0, 11133.0)
+
+  end
+
+  def test_relative
+
+    tol = Tolerance(100, :relative)
+    assert_equal 100, tol.value(1.0)
+    assert_equal 150, tol.value(1.5)
+    assert_equal 1E12, tol.value(1.0E10)
+    assert_equal 1E12, tol.value(-1.0E10)
+    assert_equal 100, tol.value(Decimal('1.0'))
+    assert_equal 150, tol.value(Decimal('1.5'))
+    assert_equal 1E12, tol.value(Decimal('1.0E10'))
+    assert_equal 1E12, tol.value(Decimal('-1.0E10'))
+
+  end
+
+  def test_floating
+
+    tol = Tolerance(100, :floating)
+    assert_equal 100, tol.value(1.0)
+    assert_equal 200, tol.value(1.5)
+    assert_equal 100*2.0**34, tol.value(1.0E10)
+    assert_equal 100*2.0**34, tol.value(-1.0E10)
+    assert_equal 100, tol.value(Decimal('1.0'))
+    assert_equal 1000, tol.value(Decimal('1.5'))
+    assert_equal 1E12, tol.value(Decimal('1.0E10'))
+    assert_equal 1E12, tol.value(Decimal('-1.0E10'))
+
+  end
+
   def test_significant_decimals
     t = SigDecimalsTolerance.new(4)
 
-    assert t.equal?(Decimal('1.2345678'), Decimal('1.235'))
-    assert t.equal?(Decimal('12345678'), Decimal('12350000'))
+    assert t.eq?(Decimal('1.2345678'), Decimal('1.235'))
+    assert t.eq?(Decimal('12345678'), Decimal('12350000'))
+    assert !t.eq?(Decimal('1.2345678'), Decimal('1.234'))
 
   end
 
   def test_ulps
     t = SigDecimalsTolerance.new(4)
 
-    assert t.equal?(Decimal('1.2345678'), Decimal('1.235'))
-    assert t.equal?(Decimal('12345678'), Decimal('12350000'))
+    assert t.eq?(Decimal('1.2345678'), Decimal('1.235'))
+    assert t.eq?(Decimal('12345678'), Decimal('12350000'))
+    assert !t.eq?(Decimal('12345678'), Decimal('12380000'))
 
   end
 
