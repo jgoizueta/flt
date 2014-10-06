7d5361118e865966d2bc1c07c29ec4930c72030b
Javier Goizueta <jgoizueta@gmail.com>
2014-10-06 16:03:35 +0200

    Fix comment typo

--- lib/flt/num.rb
+++ lib/flt/num.rb
@@ -2993,7 +2993,7 @@ class Num < Numeric
   #   :precision  1   2   3   4     5    6    7    8
   #   :power    1E3 1E2  10   1   0.1 1E-2 1E-3 1E-4
   #   :index      0   1   2   3     4    5    6    7
-  #   :index      7   6   5   4     3    2    1    0
+  #   :rindex     7   6   5   4     3    2    1    0
   def round(opt={})
     opt = { :places=>opt } if opt.kind_of?(Integer)
     r = opt[:rounding] || :half_up
